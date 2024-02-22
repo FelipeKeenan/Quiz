2e7009cbdac6c777797d33e4d66972d7a16de1db
Keenan <felipekeenan2000@gmail.com>
2024-02-22 19:29:42 -0300

    Added questions based on selected difficulty

--- questions.js
+++ questions.js
@@ -1,6 +1,7 @@
 let questions = [
+    // Perguntas nível fácil
     {
-        question: 'Em qual elemento HTML nós colocamos o javascript?',
+        question: 'Em qual elemento HTML nós colocamos o JavaScript?',
         options: [
             'tag js',
             'tag script',
@@ -10,7 +11,7 @@ let questions = [
         answer: 1
     },
     {
-        question: 'Onde é o local certo de colocar o javascript?',
+        question: 'Onde é o local certo de colocar o JavaScript?',
         options: [
             'Na tag body',
             'Na tag head',
@@ -19,7 +20,7 @@ let questions = [
         answer: 2
     },
     {
-        question: 'Qual a sintaxe correta para adicionar um arquivo javascript externo na tag script?',
+        question: 'Qual a sintaxe correta para adicionar um arquivo JavaScript externo na tag script?',
         options: [
             'name="xxx.js"',
             'src="xxx.js"',
@@ -28,7 +29,7 @@ let questions = [
         answer: 1
     },
     {
-        question: 'Um arquivo javascript externo precisa ter a tag script',
+        question: 'Um arquivo JavaScript externo precisa ter a tag script',
         options: [
             'Verdade',
             'Falso'
@@ -36,7 +37,7 @@ let questions = [
         answer: 1
     },
     {
-        question: 'Como escrever um "Olá Mundo" em um alertbox?',
+        question: 'Como escrever um "Olá Mundo" em um alert box?',
         options: [
             'alertBox("Olá Mundo")',
             'msgBox("Olá Mundo")',
@@ -46,7 +47,7 @@ let questions = [
         answer: 3
     },
     {
-        question: 'Como você cria uma função no Javascript?',
+        question: 'Como você cria uma função no JavaScript?',
         options: [
             'function:minhaFuncao()',
             'function = minhaFuncao()',
@@ -64,7 +65,7 @@ let questions = [
         answer: 2
     },
     {
-        question: 'Como escrever uma condicional IF no Javascript?',
+        question: 'Como escrever uma condicional IF no JavaScript?',
         options: [
             'if i = 5',
             'if i == 5 then',
@@ -92,4 +93,188 @@ let questions = [
         ],
         answer: 0
     },
+
+    // Perguntas nível médio
+    {
+        question: 'Qual a diferença entre "==" e "==="?',
+        options: [
+            'O "==" leva em consideração o tipo de dado',
+            'O "===" leva em consideração o tipo de dado',
+            'Não possuem diferenças'
+        ],
+        answer: 1
+    },
+    {
+        question: 'Qual método JavaScript é usado para remover o último elemento de um array?',
+        options: [
+            'pop()',
+            'remove()',
+            'splice()'
+        ],
+        answer: 0
+    },
+    {
+        question: 'O que o método map() faz em JavaScript?',
+        options: [
+            'Aplica uma função a cada elemento de um array e retorna um novo array com os resultados',
+            'Remove elementos duplicados de um array',
+            'Ordena os elementos de um array em ordem alfabética'
+        ],
+        answer: 0
+    },
+    {
+        question: 'O que é "hoisting" em JavaScript?',
+        options: [
+            'É o processo de declaração de uma variável ou função para cima do seu escopo atual',
+            'É uma função que lança exceções em JavaScript',
+            'É uma técnica de criptografia de dados'
+        ],
+        answer: 0
+    },
+    {
+        question: 'Qual a diferença entre let e var em JavaScript?',
+        options: [
+            'let declara uma variável local ao bloco onde ela é definida, enquanto var declara uma variável global',
+            'let é usado para declaração de variáveis em loops, enquanto var é usado em declarações normais',
+            'let é uma abreviação de var, e ambos são usados para declarar variáveis'
+        ],
+        answer: 0
+    },
+    {
+        question: 'O que é uma "closure" em JavaScript?',
+        options: [
+            'Uma função que tem acesso ao escopo de outra função exterior (pai) mesmo após a função pai ter sido executada',
+            'Um método usado para fechar janelas pop-up em navegadores',
+            'Um erro de sintaxe comum em loops'
+        ],
+        answer: 0
+    },
+    {
+        question: 'Qual é o resultado da expressão 5 + "5" em JavaScript?',
+        options: [
+            '"10"',
+            '10',
+            'Erro de tipo'
+        ],
+        answer: 0
+    },
+    {
+        question: 'O que é "callback hell" em JavaScript?',
+        options: [
+            'Um termo usado para descrever a situação em que várias funções de callback são aninhadas de forma excessiva, tornando o código difícil de ler e dar manutenção',
+            'Um tipo de erro de sintaxe comum em loops',
+            'Um método para chamar uma função de retorno de chamada em um intervalo específico'
+        ],
+        answer: 0
+    },
+    {
+        question: 'Qual a função do método forEach() em JavaScript?',
+        options: [
+            'Executa uma função para cada elemento de um array',
+            'Remove elementos de um array com base em uma condição especificada',
+            'Ordena os elementos de um array em ordem alfabética'
+        ],
+        answer: 0
+    },
+    //Perguntas difíceis
+    {
+        question: 'Qual é o resultado da expressão "typeoff null", em JS?',
+        options: [
+            'null',
+            'undefined',
+            'object'
+        ],
+        answer: 2
+    },
+    {
+        question: 'Como você explicaria o conceito de "closures" em JavaScript?',
+        options: [
+            'Closures são blocos de código que são executados assincronamente em segundo plano',
+            'Closures são funções que capturam variáveis de um escopo externo em que foram definidas, mesmo que esse escopo externo não exista mais',
+            'Closures são estruturas de dados para armazenar múltiplos valores em JavaScript'
+        ],
+        answer: 1
+    },
+    {
+        question: 'O que é "prototype" em JavaScript?',
+        options: [
+            'Prototype é uma função que cria novos objetos com base em um protótipo existente',
+            'Prototype é uma técnica para ocultar a implementação interna de um objeto em JavaScript',
+            'Prototype é um mecanismo pelo qual os objetos JavaScript herdam recursos uns dos outros'
+        ],
+        answer: 2
+    },
+    {
+        question: 'Qual é o resultado da expressão `[] == ![]` em JavaScript?',
+        options: [
+            'true',
+            'false',
+            'Isso lança um erro de sintaxe'
+        ],
+        answer: 0
+    },
+    {
+        question: 'Por que usar a palavra-chave `let` é preferível a `var` em muitos casos?',
+        options: [
+            '`let` cria uma variável com escopo de bloco, enquanto `var` tem escopo de função',
+            '`let` tem uma performance melhor em relação a `var`',
+            '`let` é uma palavra-chave mais moderna e recomendada pelos padrões ECMAScript mais recentes'
+        ],
+        answer: 0
+    },
+    {
+        question: 'O que é o conceito de "promises" em JavaScript?',
+        options: [
+            'Promises são objetos usados para representar o resultado de uma operação assíncrona eventualmente assíncrona',
+            'Promises são funções que são executadas após a conclusão de uma operação assíncrona',
+            'Promises são callbacks usados para lidar com exceções em operações assíncronas'
+        ],
+        answer: 0
+    },
+    {
+        question: 'Como você lidaria com a execução paralela de múltiplas operações assíncronas em JavaScript?',
+        options: [
+            'Usando callbacks aninhados para garantir que as operações sejam executadas em paralelo',
+            'Usando `Promise.all()` para esperar que todas as promessas sejam resolvidas antes de continuar',
+            'Usando uma função `parallel()` específica do JavaScript para executar operações em paralelo'
+        ],
+        answer: 1
+    },
+    {
+        question: 'O que é "currying" em JavaScript?',
+        options: [
+            'Currying é o processo de transformar uma função com múltiplos argumentos em uma sequência de funções com um único argumento',
+            'Currying é o processo de compactar código JavaScript para otimizar a performance',
+            'Currying é um conceito relacionado à criptografia de dados em JavaScript'
+        ],
+        answer: 0
+    },
+    {
+        question: 'O que é "destructuring" em JavaScript?',
+        options: [
+            'Destructuring é uma função do JavaScript para destruir objetos que não são mais necessários',
+            'Destructuring é uma medida de segurança implementada em ambientes de execução de código JavaScript',
+            'Destructuring é uma técnica que permite extrair dados de arrays e objetos usando uma sintaxe mais concisa',
+        ],
+        answer: 2
+    },
+    {
+        question: 'Qual é a diferença entre as palavras-chave `function` e `=>` para definir funções em JavaScript?',
+        options: [
+            '`=>` cria funções de seta (arrow functions) com escopo léxico, enquanto `function` cria funções com escopo dinâmico',
+            '`=>` é uma abreviação de `function` e pode ser usado de forma intercambiável',
+            '`function` é uma palavra-chave mais antiga e `=>` é uma forma mais moderna e concisa de definir funções em JavaScript'
+        ],
+        answer: 0
+    },
+    {
+        question: 'Qual é a diferença entre null e undefined em JavaScript?',
+        options: [
+            'Null é um valor que indica a ausência de um objeto, enquanto undefined é um valor primitivo que indica que uma variável não foi atribuída',
+            'Não há diferença entre eles, ambos representam a ausência de valor',
+            'Null representa a ausência intencional de qualquer valor em JavaScript, enquanto undefined é atribuído automaticamente a variáveis que não foram inicializadas',
+        ],
+        answer: 2
+    }
 ];
+
--- script.js
+++ script.js
@@ -3,17 +3,43 @@ let currentQuestion = 0;
 let correctAnswer = 0
 
 //Actions
-showQuestion()
-document.querySelector('.scoreArea button').addEventListener('click', resetQuiz)
+document.querySelector('.try-again-btn').addEventListener('click', resetQuiz)
+document.querySelector('.start-btn').addEventListener('click', startGame);
 
 
 //Functions
-function showQuestion(){ 
-    if(questions[currentQuestion]){
+
+//Start Game
+function startGame() {
+    document.querySelector(".hidden-container").style.display = 'block'
+    document.querySelector(".start-game").style.display = 'none'
+
+    //Variável que contém o nível que o usuário selecionou
+    let gameDificult = document.querySelector('.hidden-select').value
+
+    //Criando variáveis que vão conter o nível selecionado pelo usuário
+    let easyDificult = questions.slice(0, 10)
+    let mediumDificult = questions.slice(10, 20)
+    let hardDificult = questions.slice(20)
+
+    if (gameDificult === 'easy') {
+        questions = easyDificult
+    }
+    if (gameDificult === 'medium') {
+        questions = mediumDificult
+    }
+    if (gameDificult === 'hard') {
+        questions = hardDificult
+    }
+    showQuestion()
+}
+
+function showQuestion() {
+    if (questions[currentQuestion]) {
         let q = questions[currentQuestion] //Variável que vai conter o número da questão dentro do Array.
 
         //Atualizando a barra de progresso sempre que troca de questão
-        let pctBar = Math.floor((currentQuestion / questions.length) *100) //Variável que vai armazenar o tamanho da barrinha superior sempre acompanhando a questão atual (Math.floor pra arredondar os números).
+        let pctBar = Math.floor((currentQuestion / questions.length) * 100) //Variável que vai armazenar o tamanho da barrinha superior sempre acompanhando a questão atual (Math.floor pra arredondar os números).
         document.querySelector(".progress--bar").style.width = `${pctBar}%`
 
         document.querySelector(".scoreArea").style.display = 'none'
@@ -24,11 +50,11 @@ function showQuestion(){
 
         //Acessando cada "option" dentro do array através de um loop.
         let optionsHtml = ''
-        for(i in q.options){ 
+        for (i in q.options) {
             optionsHtml += `<div data-op=${i} class="option"><span>${parseInt(i) + 1}</span>${q.options[i]}</div>`
         }
 
-         //Exibindo cada "option" através de um loop.
+        //Exibindo cada "option" através de um loop.
         document.querySelector(".options").innerHTML = optionsHtml
 
         //Adicionando evento de clique nas opções
@@ -36,7 +62,7 @@ function showQuestion(){
             item.addEventListener('click', clickOptionEvent)
         })
     }
-    else{
+    else {
         //Chamando a função quando acabar as questions
         finishQuiz()
     }
@@ -44,42 +70,42 @@ function showQuestion(){
 }
 
 //Função que vai "pegar" onde eu cliquei atráves do atributo "data-op" 
-function clickOptionEvent(e){
-   let clickedOption = parseInt(e.target.getAttribute('data-op')) //Transformando a string em inteiro
+function clickOptionEvent(e) {
+    let clickedOption = parseInt(e.target.getAttribute('data-op')) //Transformando a string em inteiro
 
-   if(questions[currentQuestion].answer === clickedOption){
-        console.log("Você acertou!")
+    if (questions[currentQuestion].answer === clickedOption) {
+
 
         //Adicionando uma unidade a cada acerto de resposta.
-         correctAnswer ++;
-   } 
+        correctAnswer++;
+    }
 
-//Passando pra próxima pergunta e mostrando a função showQuestion novamente.
-   currentQuestion ++;
-   showQuestion()
-   
+    //Passando pra próxima pergunta e mostrando a função showQuestion novamente.
+    currentQuestion++;
+    showQuestion()
+
 
 }
 
 
-function finishQuiz(){
+function finishQuiz() {
     //Criando a variável que vai calcular a porcentagem de acertos
     let points = Math.floor((correctAnswer / questions.length) * 100)
 
     //Exibindo a porcentagem de acertos
     document.querySelector(".scorePct").innerHTML = `Acertou ${points}%`
-    
+
     //Exibindo o número total de acertos
     document.querySelector(".scoreText2").innerHTML = `Você acertou ${correctAnswer} de ${questions.length} questões!`
 
     //Alterando a cor e o texto conforme a performance
-    if(points < 40){
+    if (points < 40) {
         document.querySelector('.scoreText1').style.color = 'red'
-        document.querySelector('.scoreText1').innerHTML = 'Insuficiente! Estude mais e tente novamente.'
-    } else if (points >= 40 && points < 80){
+        document.querySelector('.scoreText1').innerHTML = 'Insuficiente! Estude mais e não desista!'
+    } else if (points >= 40 && points < 80) {
         document.querySelector('.scoreText1').style.color = 'yellow'
         document.querySelector('.scoreText1').innerHTML = 'Boa! Mais um pouco de estudo e você ficará muito bom!'
-    } else if (points >= 80){
+    } else if (points >= 80) {
         document.querySelector('.scoreText1').style.color = '#0d630d'
         document.querySelector('.scoreText1').innerHTML = 'Muito bem! Você obteve um excelente resultado!'
     }
@@ -90,12 +116,10 @@ function finishQuiz(){
 
     //Completando o width da barra de progresso para o máximo
     document.querySelector(".progress--bar").style.width = "100%"
-
 }
 
-function resetQuiz(){
-     currentQuestion = 0;
-     correctAnswer = 0
+function resetQuiz() {
+    currentQuestion = 0;
+    correctAnswer = 0
     showQuestion()
-
 }
